59b94050c9cc5023594d06eeb65a144b06229039
Ramya-softwareworker <ramya04032001@gmail.com>
2024-07-14 22:21:15 +0530

    Add files via upload

---
+++ Day.32/React redux task/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UserContextComponents from "./Components/UserContextComponents";
+import Cards from "./Components/Cards";
+
+export const UserContext = React.createContext();
+function App() {
+  return (
+    <div className="container">
+      <UserContextComponents>
+        <Cards />
+      </UserContextComponents>
+    </div>
+  );
+}
+export default App;
